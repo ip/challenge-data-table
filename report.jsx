c7c28c2b6cf15ff57d691f9a4dab87ce14e9312b
Ivan Perevezentsev <ivan.perevezentsev@gmail.com>
2018-03-27 23:26:21 +0200

    feat: complete the task

--- report.jsx
+++ report.jsx
@@ -1,11 +1,43 @@
-var React = require('react')
-var ReactPivot = require('react-pivot')
-var createReactClass = require('create-react-class')
+const React = require('react')
+const ReactPivot = require('react-pivot')
+const createReactClass = require('create-react-class')
 
-var rows = require('./data.json')
+const rows = require('./data.json')
 
-module.exports = createReactClass({
-  render () {
-    return <div>Report</div>
+module.exports = () =>
+  <ReactPivot {...{ rows, dimensions, reduce, calculations }} />
+
+const dimensions = [
+  { value: 'date', title: 'Date' },
+  { value: 'host', title: 'Host' }
+]
+
+const calculations = [
+  { title: 'Impressions', value: 'impressions' },
+  { title: 'Loads', value: 'loads' },
+  { title: 'Displays', value: 'displays' },
+  {
+    title: 'Load Rate',
+    value: row => `${(row.loads / row.impressions * 100).toFixed(1)}%`,
+  },
+  {
+    title: 'Display Rate',
+    value: row => `${(row.displays / row.loads * 100).toFixed(1)}%`,
   }
-})
+]
+
+function reduce (row, accumulator) {
+  const acc = accumulator
+
+  ;['impressions', 'loads', 'displays'].forEach(
+    type => acc[type] = acc[type] || 0
+  )
+
+  ;['impression', 'load', 'display'].forEach(type => {
+    if (row.type === type) {
+      acc[`${type}s`]++
+    }
+  })
+
+  return acc
+}
